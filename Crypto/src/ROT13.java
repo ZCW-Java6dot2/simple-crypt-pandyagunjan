9a31d4a09fda2e65a324276bc08c7f392510e0ff
gunjan <pandya.gunjan@gmail.com>
2020-11-30 14:01:08 -0500

    done rotate

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,30 +3,45 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+   private Character cs;
+    private Character cf;
 
     ROT13(Character cs, Character cf) {
+        this.cs=cs;
+        this.cf=cf;
     }
 
     ROT13() {
+        cs='a';
+        cf='n';
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+     char[] textAsArray= text.toCharArray();
+
 
         return "";
     }
 
     public String encrypt(String text) {
-        return text;
+
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        Integer indexOfChar = 0;
+        StringBuilder bufferString = new StringBuilder();
+        indexOfChar = s.indexOf(c); //index of the character
+        bufferString.append(s.substring(indexOfChar,s.length())); //copy from index to end
+        bufferString.append(s.substring(0,indexOfChar)); //copy from 0 to index
+        return bufferString.toString();
 
-        return "";
     }
 
 }
+
